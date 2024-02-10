6ddf43bbe455a7e63d7fb1697f31bcb46300f335
EdmiltonPeixeira <edm.ufpasi@gmail.com>
2024-02-09 22:44:05 -0300

    commit inicial

---
+++ src/test/java/com/edmilton/EPayment/EPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edmilton.EPayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
