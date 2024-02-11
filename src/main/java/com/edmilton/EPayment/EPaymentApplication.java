7bd9de3216a91c40204ac0b880811a991fbe27a9
EdmiltonPeixeira <edm.ufpasi@gmail.com>
2024-02-10 21:02:28 -0300

    (feat): adicionando configurações para documentar api com swagger

--- src/main/java/com/edmilton/EPayment/EPaymentApplication.java
+++ src/main/java/com/edmilton/EPayment/EPaymentApplication.java
@@ -1,9 +1,23 @@
 package com.edmilton.EPayment;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "EPayment",
+				version = "1.0.0",
+				description = "API Rest para gerenciamento de pagamentos",
+				contact = @Contact(
+						name = "Edmilton Ribeiro Peixeira | Desenvolvedor",
+						email = "edm.ufpasi@gmail.com"
+				)
+		)
+)
 public class EPaymentApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/edmilton/EPayment/controller/PagamentoController.java
+++ src/main/java/com/edmilton/EPayment/controller/PagamentoController.java
@@ -3,6 +3,7 @@ package com.edmilton.EPayment.controller;
 import com.edmilton.EPayment.dto.AtualizaPagamentoDto;
 import com.edmilton.EPayment.model.Pagamento;
 import com.edmilton.EPayment.service.PagamentoService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,23 +21,28 @@ public class PagamentoController {
 
     @GetMapping("/todos")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Exibe todos os pagamentos cadastrados.")
     public List<Pagamento> listarTodos(){
         return pagamentoService.listarTodosPagamentos();
     }
 
     @PostMapping
     @Transactional
+    @Operation(summary = "Cadastra um novo pagamento.")
     public ResponseEntity<String> cadastrar(@RequestBody Pagamento pagamento){
         return pagamentoService.cadastrarPagamento(pagamento);
     }
 
     @PutMapping
     @Transactional
-    public ResponseEntity<String> atualizar(@RequestBody AtualizaPagamentoDto atualizaPagamentoDto){
+    @Operation(summary = "Altera o status de um pagamento.")
+    public ResponseEntity<String> atualizarStatus(@RequestBody AtualizaPagamentoDto atualizaPagamentoDto){
         return pagamentoService.alterarPagamento(atualizaPagamentoDto);
     }
 
     @GetMapping("/filtros")
+    @Operation(summary = "Exibe os pagamentos cadastrados a partir dos filtros: " +
+            "codigoDebito, cpfCnpjPagador e status.")
     public List<Pagamento> listarComFiltros(
             @RequestParam(name = "codigoDebito", required = false) Integer codigoDebito,
             @RequestParam(name = "cpfCnpjPagador", required = false) String cpfCnpjPagador,
@@ -46,6 +52,7 @@ public class PagamentoController {
 
     @DeleteMapping
     @Transactional
+    @Operation(summary = "Exclui um pagamento a partir do seu id.")
     public ResponseEntity<String> excluir(@RequestParam(name = "idPagamento", required = false) Integer idPagamento){
         return pagamentoService.excluirPagamento(idPagamento);
     }
